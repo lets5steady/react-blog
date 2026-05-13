69c66f280cfd753eab5f0c1d0da3f9b429d0f54e
lets5steady <lets.05.steady@gmail.com>
2026-05-13 16:33:53 +0900

    005_FirstViewコンポーネント作成

---
+++ src/components/header/FirstView/FirstView.jsx
@@ -0,0 +1,10 @@
+import styles from './FirstView.module.css'
+import logo from '../../../assets/bamosDesignLogo_white.svg'
+
+export default function FirstView() {
+    return(
+    <div className={styles.container}>
+        <div className={styles.item}> <img src={logo} alt="ロゴ" /> </div>
+    </div>
+    );
+}
