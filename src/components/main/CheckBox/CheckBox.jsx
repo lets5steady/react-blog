e923c3ce1200146232ca974b51d40e2fd6ce8a9c
lets5steady <lets.05.steady@gmail.com>
2026-05-15 15:59:27 +0900

    014_CheckBoxコンポーネント作成（inputの値を一括管理する仕組みは未実装）

--- src/components/main/CheckBox/CheckBox.jsx
+++ src/components/main/CheckBox/CheckBox.jsx
@@ -6,22 +6,33 @@ export default function CheckBox({ categoryName, options=[]}) {
     const [form,setForm] = useState({});
 
     const handleForm = e => {
+        const checkedItem = [];
+        
+        if (e.target.checked) {
+            checkedItem.push(e.target.value);
+        }else{
+            checkedItem.splice(checkedItem.indexOf(e.target.value), 1);
+        }
+
         setForm({
             ...form,
-            [e.target.name]: e.target.value
+            [e.target.name]: checkedItem
         });
+        console.log(checkedItem);
+        
     }
 
     return(
-            options.map(option => 
+            <>
+            {options.map(option => 
                 <label key={option.id}>
-                    <input type="check" name={categoryName} id={option.id}
+                    <input type="checkbox" name={categoryName} id={option.id}
                         value={option.value}
                         onChange={handleForm}>
                     </input>
                     {option.text}
-                </label>)
-
+                </label>)}
+            </>
     );
 }
 
