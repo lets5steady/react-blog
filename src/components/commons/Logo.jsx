a1dd2e6569cd767b6177c7f07a9dd353915e48ce
lets5steady <lets.05.steady@gmail.com>
2026-05-16 13:16:42 +0900

    021_Logoのタグ名をprops化
    サイト内に<h1>に相当するものがなく、ヘッダーロゴを<h1>とするため、ロゴ生成時のタグを可変とした。

--- src/components/commons/Logo.jsx
+++ src/components/commons/Logo.jsx
@@ -2,14 +2,16 @@ import logoBlack from '../../assets/bamosDesignLogo_black.svg'
 import logoWhite from '../../assets/bamosDesignLogo_white.svg'
 import logoAccent from '../../assets/bamosDesignLogo_accent.svg'
 
-export default function Logo({ color = 'black' }) {
+// ロゴを囲むタグをdiv以外（h1など）にも変更できるようタグ名をpropsで受け取る
+// 小文字だと<tag>になってしまうので大文字にして変数であることを明示
+export default function Logo({ color = 'black' ,Tag = 'div' }) {
             if (color === 'black') {
-                return <div><img src={logoBlack} alt="黒のロゴ" /> </div>
+                return <Tag><img src={logoBlack} alt="黒のロゴ" /></Tag>
             }
             if (color === 'white') {
-                return <div><img src={logoWhite} alt="白のロゴ" /> </div>
+                return <Tag><img src={logoWhite} alt="白のロゴ" /></Tag>
             }
             if (color === 'accent') {
-                return <div><img src={logoAccent} alt="ピンクのロゴ" /> </div>
+                return <Tag><img src={logoAccent} alt="ピンクのロゴ" /></Tag>
             }
 };
