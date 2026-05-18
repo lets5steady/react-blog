ded262d60caaa6e12908c642defa2934fa51629e
lets5steady <lets.05.steady@gmail.com>
2026-05-18 21:33:12 +0900

    026_PlusButtonのimgをインラインSVGに変更、aria-label追加。
    ドロワーメニューの開閉状態に合わせてカラー変更できるように挿入方法を変更
    imgタグのaltに開閉できる旨を記載していたが、buttonタグに変更し、ラベルの内容も状態に合わせて変わるように改善

--- src/components/header/PlusButton/PlusButton.jsx
+++ src/components/header/PlusButton/PlusButton.jsx
@@ -1,14 +1,18 @@
 import styles from './PlusButton.module.css';
-import plus from '../../../assets/plus.png';
 
 export default function PlusButton({text  = {} , isOpen , onToggle }) {
 
     return(
         <>
             <button  className={styles.container}
-            onClick={onToggle}>
-                <img src={plus} alt="開閉するメニューボタン" className={isOpen ? `${styles.close}` : `${styles.btn}`}/>
-                <span>{isOpen ? text.active : text.nomal}</span>
+            onClick={onToggle}
+            aria-label={isOpen ? 'メニューを閉じる' : 'メニューを開く'}>
+                <svg className={isOpen ? `${styles.close}` : `${styles.btn}`} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect y="8" width="20" height="4" fill='currentColor'/>
+                <rect x="8" y="20" width="20" height="4" transform="rotate(-90 8 20)" fill='currentColor'/>
+                </svg>
+
+                <span className={isOpen ? styles.active : undefined}>{isOpen ? text.active : text.nomal}</span>
             </button>
         </>
     );
