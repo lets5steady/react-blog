fcc1bba4b073085383a83c59b553e0fa63b9d421
lets5steady <lets.05.steady@gmail.com>
2026-05-19 16:56:56 +0900

    028_ThumbsButtonsコンポーネント作成（ThumbsUp+ThumbsDown）

---
+++ src/components/main/ThumbsButtons/ThumbsButtons.jsx
@@ -0,0 +1,14 @@
+import styles from './ThumbsButtons.module.css';
+import ThumbsUpButton from '../ThumbsUpButton/ThumbsUpButton';
+import ThumbsDownButton from "../ThumbsDownButton/ThumbsDownButton"
+
+
+export default function ThumbsButtons({thumbsUp = 0 , thumbsDown = 0}) {
+
+    return (
+        <div className={styles.actions}>
+            <ThumbsUpButton initialValue={thumbsUp}/>
+            <ThumbsDownButton initialValue={thumbsDown}/>
+        </div>
+    );
+}
