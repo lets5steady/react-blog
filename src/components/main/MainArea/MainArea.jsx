bd98ca987cef79b9a359f11b4c7363148a0cbe14
lets5steady <lets.05.steady@gmail.com>
2026-05-22 16:02:13 +0900

    033_Header~Mainの要素間の間隔調整
    FirstViewのSP版の画像サイズ、ドロワーメニュー周りは要調整

--- src/components/main/MainArea/MainArea.jsx
+++ src/components/main/MainArea/MainArea.jsx
@@ -1,11 +1,15 @@
 import CardItem from '../CardItem/CardItem';
 import styles from './MainArea.module.css';
 import bamosList from '../../../../bamosList';
+import SectionHeader from '../../commons/SectionHeader/SectionHeader';
 
 export default function MainArea() {
     return(
         <main>
-            <CardItem itemData={bamosList}/>
+            <SectionHeader title='Latest Blog Posts'/>
+            <div className={styles.list}>
+                <CardItem itemData={bamosList}/>
+            </div>
         </main>
     );
 }
